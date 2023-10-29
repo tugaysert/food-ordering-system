07d621ee8df103003c0aa0cddf3406802332cf2b
Tugay Sert <tugay.sert@eteration.com>
2023-10-29 18:34:29 +0300

    Method impl for order entity

--- food-ordering-system/order-service/order-domain/order-domain-core/src/main/java/org/example/order/service/domain/entity/Order.java
+++ food-ordering-system/order-service/order-domain/order-domain-core/src/main/java/org/example/order/service/domain/entity/Order.java
@@ -1,11 +1,14 @@
 package org.example.order.service.domain.entity;
 
 import org.example.entity.AggregateRoot;
+import org.example.order.service.domain.exception.OrderDomainException;
+import org.example.order.service.domain.valueobject.OrderItemId;
 import org.example.order.service.domain.valueobject.StreetAddress;
 import org.example.order.service.domain.valueobject.TrackingId;
 import org.example.valueobject.*;
 
 import java.util.List;
+import java.util.UUID;
 
 public class Order extends AggregateRoot<OrderId> {
   private final CustomerId customerId;
@@ -16,6 +19,61 @@ public class Order extends AggregateRoot<OrderId> {
   private TrackingId trackingId;
   private OrderStatus orderStatus;
   private List<String> failureMessages;
+
+  public void intializeOrder() {
+    setId(new OrderId(UUID.randomUUID()));
+    trackingId = new TrackingId(UUID.randomUUID());
+    orderStatus = OrderStatus.PENDING;
+    initializeOrderItems();
+
+  }
+
+  public void validateOrder() {
+    validateInitialOrder();
+    validateTotalPrice();
+    validateItemsPrice();
+
+  }
+
+
+  private void validateInitialOrder() {
+    if (orderStatus != null || getId() != null) {
+      throw new OrderDomainException("Order is not in correct state for initialization.");
+    }
+  }
+
+  private void validateTotalPrice() {
+    if (price == null || !price.isGreaterThanZero()) {
+      throw new OrderDomainException("Total price must be greater than zero!");
+    }
+  }
+
+  private void validateItemsPrice() {
+    Money orderItemsTotal = items.stream().map(orderItem -> {
+      validateItemPrice(orderItem);
+      return orderItem.getSubTotal();
+    }).reduce(Money.ZERO, Money::add);
+
+    if (!price.equals(orderItemsTotal)) {
+      throw new OrderDomainException("Total price: " + price.getAmount() +
+                                     " is not equal to Order items total: " + orderItemsTotal.getAmount() + "!");
+    }
+  }
+
+  private void validateItemPrice(OrderItem orderItem) {
+    if (!orderItem.isPriceValid()) {
+      throw new OrderDomainException("Order item price: " + orderItem.getPrice().getAmount() +
+                                     " is not valid for product " + orderItem.getProduct().getId().getValue());
+    }
+  }
+
+
+  private void initializeOrderItems() {
+    long itemId = 1;
+    for (OrderItem orderItem : items) {
+      orderItem.initializeOrderItem(super.getId(), new OrderItemId(itemId++));
+    }
+  }
 
   private Order(Builder builder) {
     super.setId(builder.orderId);
