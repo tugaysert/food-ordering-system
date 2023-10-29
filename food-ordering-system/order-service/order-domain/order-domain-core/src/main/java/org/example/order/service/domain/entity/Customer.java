867132105921877fcf1da6fb25b6fda28bd5740b
Tugay Sert <tugay.sert@eteration.com>
2023-10-29 18:48:34 +0300

    add mutable domain logic

---
+++ food-ordering-system/order-service/order-domain/order-domain-core/src/main/java/org/example/order/service/domain/entity/Customer.java
@@ -0,0 +1,7 @@
+package org.example.order.service.domain.entity;
+
+import org.example.entity.AggregateRoot;
+import org.example.valueobject.CustomerId;
+
+public class Customer extends AggregateRoot<CustomerId> {
+}
